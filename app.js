8c73df7e103b0bc82522f08f858a3905cfd41b2b
Honey Sanghani <honeysanghani98@gmail.com>
2021-08-22 03:23:02 -0400

    html file created

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const portfolioDataArgs = process.argv.slice(2,process.argv.length);
-const printPortfolioData=(profileDataArr)=>{
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-      }
-    
-      console.log('================');
-    
-      // Is the same as this...
-      profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-      });
-}
-printPortfolioData(portfolioDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name,github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
